5e2a87dbef2f7c58ca7de24ae4d66e9c0c61923f
jozivam P. da silva <28790059+jozivam@users.noreply.github.com>
2023-10-10 23:45:42 +0000

    sdd

--- PYTHON/ATV3_OG_ORDENACAO/CLASS/Ex1.py
+++ PYTHON/ATV3_OG_ORDENACAO/CLASS/Ex1.py
@@ -7,12 +7,3 @@ def ordena(lista):
                 (lista[j], lista[j+1]) = (lista[j+1], lista[j])
     return lista
 
-class indiceM:
-    def indiceMenor(lista, inicio):
-        minimo = inicio
-
-        n = len(lista)
-        for i in range(inicio + 1, n):
-            if lista[minimo] > lista[i]:
-                minimo = i
-        return minimo
--- PYTHON/ATV3_OG_ORDENACAO/main.py
+++ PYTHON/ATV3_OG_ORDENACAO/main.py
@@ -1,6 +1,7 @@
 import CLASS.Ex1 as ex1
 
 
+#Bubble Sort
 lista = [12,20,1,6,7,0,200,3]
 o = ex1.ordena(lista)
 print(o)
